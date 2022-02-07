886e232610edea372ea90733e7ae9029a5bcfe9a
Ed Barberis <ed.barberis@gmail.com>
2022-02-07 16:27:53 -0500

    Upgraded Terraform to 1.1.5; and provider for AWS to 3.74.

--- builders/terraform/aws/fso-lab/main.tf
+++ builders/terraform/aws/fso-lab/main.tf
@@ -90,7 +90,7 @@ data "aws_security_group" "eks_remote" {
 # Modules ------------------------------------------------------------------------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 3.11"
+  version = ">= 3.12"
 
   name = local.vpc_name
   cidr = var.aws_vpc_cidr_block
