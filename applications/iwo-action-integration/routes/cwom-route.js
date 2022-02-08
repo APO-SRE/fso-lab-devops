bfd4edd208707dfcb9db1f0efc72559ed22671f7
Ed Barberis <ed.barberis@gmail.com>
2022-02-08 17:12:57 -0500

    Added IWO Action Integration Node.js application.

---
+++ applications/iwo-action-integration/routes/cwom-route.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var CWOMService = require('../src/CWOMService.js');
+var cwomsvc = new CWOMService({});
+
+
+router.get('/actions/:id', function(req, res) {
+    var id = req.params.id;
+    cwomsvc.getTurboActions(id).then((actions) => {
+        var results  = {
+            cwomserver: cwomsvc.config.iwoserver,
+            actions : actions
+        }
+        res.status(200).send(results);
+    } , (err) => {
+        res.status(500).send({});
+    });
+});
+
+router.get('/mockData', function(req, res) {
+    cwomsvc.getTurboActionListMockData(false).then((actions) => {
+        var results  = {
+            cwomserver: cwomsvc.config.turboserver,
+            actions : actions,
+            
+        }
+        res.status(200).send(results);
+    } , (err) => {
+        res.status(500).send({});
+    });
+    
+
+	
+});
+
+module.exports = router;
