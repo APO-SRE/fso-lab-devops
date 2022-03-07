000924f3e3559da3214feead92a47507d85ddda3
Ed Barberis <ed.barberis@gmail.com>
2022-03-07 14:27:26 -0500

    Upgraded Terraform to 1.1.7; and provider for AWS to 4.4.

--- builders/terraform/aws/fso-lab/versions.tf
+++ builders/terraform/aws/fso-lab/versions.tf
@@ -1,11 +1,11 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.1.6"
+  required_version = ">= 1.1.7"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.2"
+      version = ">= 4.4"
     }
 
     kubernetes = {
--- provisioners/scripts/common/install_hashicorp_terraform.sh
+++ provisioners/scripts/common/install_hashicorp_terraform.sh
@@ -28,9 +28,9 @@
 #---------------------------------------------------------------------------------------------------
 
 # install hashicorp terraform. ---------------------------------------------------------------------
-terraform_release="1.1.6"
+terraform_release="1.1.7"
 terraform_binary="terraform_${terraform_release}_linux_amd64.zip"
-terraform_sha256="3e330ce4c8c0434cdd79fe04ed6f6e28e72db44c47ae50d01c342c8a2b05d331"
+terraform_sha256="e4add092a54ff6febd3325d1e0c109c9e590dc6c38f8bb7f9632e4e6bcca99d4"
 
 # create local bin directory (if needed).
 mkdir -p /usr/local/bin
