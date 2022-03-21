0050b2b367ad6737ddd53ef8417f306a8a78fa12
Ed Barberis <ed.barberis@gmail.com>
2022-03-21 15:29:14 -0400

    Updated FSO Lab User create and delete scripts.

--- provisioners/scripts/aws/delete_aws_fso_lab_user.sh
+++ provisioners/scripts/aws/delete_aws_fso_lab_user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 #---------------------------------------------------------------------------------------------------
-# Create FSO Lab User with associated Group and Policies.
+# Delete FSO Lab User with associated Group and Policies.
 #
 # An AWS Identity and Access Management (IAM) user is an entity that you create in AWS to represent
 # the person or application that uses it to interact with AWS. A user in AWS consists of a name and
@@ -24,7 +24,6 @@
 # set default values for input environment variables if not set. -----------------------------------
 # [OPTIONAL] aws create user install parameters [w/ defaults].
 aws_user_name="${aws_user_name:-fso-lab-user}"
-aws_user_password="${aws_user_password:-C1sc0Fso2022!!}"
 aws_group_name="${aws_group_name:-fso-lab-group}"
 aws_policy_name="${aws_policy_name:-FSOLabTEAgentDeployment}"
 local_devops_home="${local_devops_home:-${HOME}/fso-lab-devops}"
@@ -39,7 +38,6 @@ Usage:
 
   Description of Environment Variables:
     [ubuntu]$ export aws_user_name="fso-lab-user"               # [optional] fso lab user name.
-    [ubuntu]$ export aws_user_password="<custom_password_here>" # [optional] fso lab user password.
     [ubuntu]$ export aws_group_name="fso-lab-group"             # [optional] fso lab group name.
     [ubuntu]$ export aws_policy_name="FSOLabTEAgentDeployment"  # [optional] aws policy name for thousandeyes agent.
     [ubuntu]$ export local_devops_home="${HOME}/fso-lab-devops" # [optional] local devops home environment variable.
@@ -50,65 +48,66 @@ EOF
 }
 
 # validate environment variables. ------------------------------------------------------------------
-# check if aws group already exists.
-aws_group=$(aws iam list-groups | jq -r --arg AWS_GROUP_NAME "${aws_group_name}" '.Groups[] | select(.GroupName | contains($AWS_GROUP_NAME)) | .GroupName')
+# check if aws user exists.
+aws_user=$(aws iam list-users | jq -r --arg AWS_USER_NAME "${aws_user_name}" '.Users[] | select(.UserName | contains($AWS_USER_NAME)) | .UserName')
 
-if [ ! -z "$aws_group" ]; then
-  echo "Error: aws_group_name: ${aws_group_name} already exists."
+if [ -z "$aws_user" ]; then
+  echo "Error: aws_user_name: ${aws_user_name} does NOT exist."
   usage
   exit 1
 fi
 
-# check if custom thousandeyes agent deployment policy already exists.
+# check if custom thousandeyes agent deployment policy exists.
 # retrieve custom thousandeyes agent deployment policy arn.
 aws_policy_arn=$(aws iam list-policies --scope Local --query "Policies[?PolicyName=='${aws_policy_name}'].Arn" --output json | jq -r '.[]')
 
-if [ ! -z "$aws_policy_arn" ]; then
-  echo "Error: aws_policy_name: ${aws_policy_name} already exists."
+if [ -z "$aws_policy_arn" ]; then
+  echo "Error: aws_policy_name: ${aws_policy_name} does NOT exist."
   usage
   exit 1
 fi
 
-# check if custom thousandeyes agent deployment policy template file exists.
-if [ ! -f "${local_devops_home}/provisioners/scripts/aws/policies/FSOLabTEAgentDeployment.json.template" ]; then
-  echo "Error: Can't find ${local_devops_home}/provisioners/scripts/aws/policies/FSOLabTEAgentDeployment.json.template file."
+# check if custom thousandeyes agent deployment policy file exists.
+if [ ! -f "${local_devops_home}/provisioners/scripts/aws/policies/${aws_policy_name}.json" ]; then
+  echo "Error: Can't find ${local_devops_home}/provisioners/scripts/aws/policies/${aws_policy_name}.json file."
   usage
   exit 1
 fi
 
-# check if aws user already exists.
-aws_user=$(aws iam list-users | jq -r --arg AWS_USER_NAME "${aws_user_name}" '.Users[] | select(.UserName | contains($AWS_USER_NAME)) | .UserName')
+# check if aws group exists.
+aws_group=$(aws iam list-groups | jq -r --arg AWS_GROUP_NAME "${aws_group_name}" '.Groups[] | select(.GroupName | contains($AWS_GROUP_NAME)) | .GroupName')
 
-if [ ! -z "$aws_user" ]; then
-  echo "Error: aws_user_name: ${aws_user_name} already exists."
+if [ -z "$aws_group" ]; then
+  echo "Error: aws_group_name: ${aws_group_name} does NOT exist."
   usage
   exit 1
 fi
 
-# create fso lab group and attach group policies. --------------------------------------------------
-# create fso lab group.
-aws iam create-group --group-name ${aws_group_name}
+# remove user from fso lab group and delete fso lab user. ------------------------------------------
+aws iam remove-user-from-group --group-name ${aws_group_name} --user-name ${aws_user_name}
 
-# create custom thousandeyes agent deployment policy.
-cd ${local_devops_home}/provisioners/scripts/aws/policies/
-aws_account_id=$(aws sts get-caller-identity --query "Account" --output text)
-sed -e "s/AWS_ACCOUNT_ID/${aws_account_id}/g" FSOLabTEAgentDeployment.json.template >| ${aws_policy_name}.json
-aws iam create-policy --policy-name ${aws_policy_name} --policy-document file://${aws_policy_name}.json
+# delete fso lab user.
+aws iam delete-login-profile --user-name ${aws_user_name}
+aws iam delete-user --user-name ${aws_user_name}
+
+# detach group policies and delete fso lab group. --------------------------------------------------
+# retrieve custom thousandeyes agent deployment policy arn.
 aws_policy_arn=$(aws iam list-policies --scope Local --query "Policies[?PolicyName=='${aws_policy_name}'].Arn" --output json | jq -r '.[]')
 
-# attach group policies to fso lab group.
-aws iam attach-group-policy --group-name ${aws_group_name} --policy-arn ${aws_policy_arn}
-aws iam attach-group-policy --group-name ${aws_group_name} --policy-arn arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess
-aws iam attach-group-policy --group-name ${aws_group_name} --policy-arn arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess
-aws iam attach-group-policy --group-name ${aws_group_name} --policy-arn arn:aws:iam::aws:policy/AWSCloud9EnvironmentMember
+# detach group policies from fso lab group.
+aws iam detach-group-policy --group-name ${aws_group_name} --policy-arn ${aws_policy_arn}
+aws iam detach-group-policy --group-name ${aws_group_name} --policy-arn arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess
+aws iam detach-group-policy --group-name ${aws_group_name} --policy-arn arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess
+aws iam detach-group-policy --group-name ${aws_group_name} --policy-arn arn:aws:iam::aws:policy/AWSCloud9EnvironmentMember
 
-# create fso lab user and add to fso lab group. ----------------------------------------------------
-# create fso lab user.
-aws iam create-user --user-name ${aws_user_name}
-aws iam create-login-profile --user-name ${aws_user_name} --password "${aws_user_password}" --no-password-reset-required
+# delete custom thousandeyes agent deployment policy.
+aws iam delete-policy --policy-arn ${aws_policy_arn}
 
-# add fso lab user to fso lab group.
-aws iam add-user-to-group --group-name ${aws_group_name} --user-name ${aws_user_name}
+# delete custom thousandeyes agent deployment policy file.
+rm -f "${local_devops_home}/provisioners/scripts/aws/policies/${aws_policy_name}.json"
+
+# delete fso lab group.
+aws iam delete-group --group-name ${aws_group_name}
 
 # print completion message.
-echo "FSO Lab User creation complete."
+echo "FSO Lab User deletion complete."
