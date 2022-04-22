cf84a397613ba0618ad896b8fc3a1c0d809a5851
Ed Barberis <ed.barberis@gmail.com>
2022-04-22 14:09:45 -0400

    Upgraded developer utilities to latest versions.

--- builders/terraform/aws/fso-lab/versions.tf
+++ builders/terraform/aws/fso-lab/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.10"
+      version = ">= 4.11"
     }
 
     kubernetes = {
--- provisioners/scripts/common/install_aws_eksctl_cli.sh
+++ provisioners/scripts/common/install_aws_eksctl_cli.sh
@@ -14,9 +14,9 @@
 #---------------------------------------------------------------------------------------------------
 
 # install eksctl cli. ------------------------------------------------------------------------------
-eksctl_release="0.93.0"
+eksctl_release="0.94.0"
 eksctl_binary="eksctl_$(uname -s)_amd64.tar.gz"
-eksctl_sha256="7f27988d6aa4fb8041d95d6de270b9657f605a79edd317cc044dc80c49a512e8"
+eksctl_sha256="959556b05f72d3b9bae3f7b29e4aaf22b4e878bd039e2513a7db17a220166705"
 
 # create local bin directory (if needed).
 mkdir -p /usr/local/bin
