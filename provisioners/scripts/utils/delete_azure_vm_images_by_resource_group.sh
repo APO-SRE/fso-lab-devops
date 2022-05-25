f1db5386bdd9eca1e31d106ed35d8988250161e9
Ed Barberis <ed.barberis@gmail.com>
2022-05-25 17:35:56 -0400

    Upgraded developer utilities to latest versions.

--- provisioners/scripts/utils/delete_azure_vm_images_by_resource_group.sh
+++ provisioners/scripts/utils/delete_azure_vm_images_by_resource_group.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 #---------------------------------------------------------------------------------------------------
-# Delete the FSO Lab DevOps project-specific Azure Images's by resource group.
+# Delete the AppDynamics Cloud Kickstart project-specific Azure Images's by resource group.
 #
 # NOTE: All inputs are defined by external environment variables.
 #       Optional variables have reasonable defaults, but you may override as needed.
@@ -9,8 +9,9 @@
 #---------------------------------------------------------------------------------------------------
 
 # set default values for input environment variables if not set. -----------------------------------
-appd_project_image_types="${appd_project_image_types-FSO-Lab-VM-CentOS}"
-azurerm_resource_group_name="${azurerm_resource_group_name-FSO-Lab-DevOps-Images}"
+appd_project_image_types="${appd_project_image_types-APM-Platform LPAD TeaStore}"
+azurerm_resource_group_name="${azurerm_resource_group_name-Cloud-Kickstart-Workshop-Images}"
+azurerm_image_gallery_name="${azurerm_image_gallery_name-CloudKickstartWorkshopGallery }"
 azurerm_image_keep_last="${azurerm_image_keep_last-true}"
 
 # check if 'jq' is installed. ----------------------------------------------------------------------
@@ -25,6 +26,35 @@ fi
 # initialize project image types array.
 appd_project_image_types_array=( ${appd_project_image_types} )
 appd_project_image_types_array_length=${#appd_project_image_types_array[@]}
+
+# loop for each project image type.
+for image_type in "${appd_project_image_types_array[@]}"; do
+  # retrieve azure shared image gallery version data.
+  azurerm_sig_versions=$(az sig image-version list --resource-group ${azurerm_resource_group_name} --gallery-name ${azurerm_image_gallery_name} --gallery-image-definition ${image_type}-CentOS79 | jq -rs '.[] | sort_by(.publishingProfile.publishedDate) | .[] | {name: .name} | .[] | values')
+
+  # build individual azure shared image gallery versions arrays.
+  azurerm_sig_versions_array=( $azurerm_sig_versions )
+  azurerm_sig_versions_array_length=${#azurerm_sig_versions_array[@]}
+
+  if [ "$azurerm_image_keep_last" == "true" ]; then
+    azurerm_sig_versions_array_length=$(($azurerm_sig_versions_array_length - 1))
+  fi
+
+  # loop for each shared image gallery version.
+  ii=0
+  for sig_version in "${azurerm_sig_versions_array[@]}"; do
+    if [ "$ii" -lt ${azurerm_sig_versions_array_length} ]; then
+      echo "Deleting Azure Shared Image Gallery version: '${image_type}-CentOS79:${sig_version}':"
+      echo "  az sig image-version delete --resource-group ${azurerm_resource_group_name} --gallery-name ${azurerm_image_gallery_name} --gallery-image-definition ${image_type}-CentOS79 --gallery-image-version ${sig_version}"
+      az sig image-version delete --resource-group ${azurerm_resource_group_name} --gallery-name ${azurerm_image_gallery_name} --gallery-image-definition ${image_type}-CentOS79 --gallery-image-version ${sig_version}
+    fi
+
+    # increment array index.
+    ii=$(($ii + 1))
+  done
+done
+
+echo ""
 
 # loop for each project image type.
 for image_type in "${appd_project_image_types_array[@]}"; do
