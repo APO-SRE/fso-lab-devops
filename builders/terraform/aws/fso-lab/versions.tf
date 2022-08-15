fcd6e0c5144f462d1dbcdcebb24eff43fab323c1
Ed Barberis <ed.barberis@gmail.com>
2022-08-15 13:29:32 -0400

    Upgraded Terraform to 1.2.7; AWS to 4.26; and Intersight to 1.0.31.

--- builders/terraform/aws/fso-lab/versions.tf
+++ builders/terraform/aws/fso-lab/versions.tf
@@ -1,11 +1,11 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.2.6"
+  required_version = ">= 1.2.7"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.25"
+      version = ">= 4.26"
     }
 
     kubernetes = {
