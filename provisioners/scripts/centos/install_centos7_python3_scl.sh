24f70061d8794dc2c18a2a6c77c72fbe4e2c9b17
Ed Barberis <ed.barberis@gmail.com>
2023-03-14 16:30:39 -0400

    Standardized Packer AMI builds across OS environments.

--- provisioners/scripts/centos/install_centos7_python3_scl.sh
+++ provisioners/scripts/centos/install_centos7_python3_scl.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -eux
 # install python 3.8 from the software collection library for linux 7.x..
 
-# set default value for appdynamics cloud kickstart home environment variable if not set. ----------
-kickstart_home="${kickstart_home:-/opt/appd-cloud-kickstart}"    # [optional] kickstart home (defaults to '/opt/appd-cloud-kickstart').
+# set default value for fso lab devops home environment variable if not set. -----------------------
+devops_home="${devops_home:-/opt/fso-lab-devops}"   # [optional] fso lab devops home folder (defaults to '/opt/fso-lab-devops').
 
 # create scripts directory (if needed). ------------------------------------------------------------
-mkdir -p ${kickstart_home}/provisioners/scripts/centos
-cd ${kickstart_home}/provisioners/scripts/centos
+mkdir -p ${devops_home}/provisioners/scripts/centos
+cd ${devops_home}/provisioners/scripts/centos
 
 # install python 3.8. ------------------------------------------------------------------------------
 yum -y install rh-python38
@@ -17,7 +17,7 @@ scl enable rh-python38 -- python --version
 # install python 3.8 pip. --------------------------------------------------------------------------
 rm -f get-pip.py
 wget --no-verbose https://bootstrap.pypa.io/get-pip.py
-scl enable rh-python38 -- python ${kickstart_home}/provisioners/scripts/centos/get-pip.py
+scl enable rh-python38 -- python ${devops_home}/provisioners/scripts/centos/get-pip.py
 
 # verify installation.
 scl enable rh-python38 -- pip --version
