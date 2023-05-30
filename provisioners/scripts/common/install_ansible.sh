4871a81284a6dd75a53843a5303374b3d353102f
Ed Barberis <ed.barberis@gmail.com>
2023-05-30 14:26:26 -0400

    Added support for the new AppDynamics Ansible Collection for Agent Management.

--- provisioners/scripts/common/install_ansible.sh
+++ provisioners/scripts/common/install_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -eux
-# install ansible 2.x with pip3 linux 202x.
+# install ansible 2.x with python3 for linux.
 
 # set default values for input environment variables if not set. -----------------------------------
 # [OPTIONAL] ansible 2.x install parameters [w/ defaults].
@@ -24,7 +24,7 @@ if [ -z "$user_name" ]; then
   exit 1
 fi
 
-if [ "$user_name" == "root" ]; then
+if [ "$user_name" = "root" ]; then
   echo "Error: 'user_name' should NOT be 'root'."
   usage
   exit 1
@@ -33,4 +33,7 @@ fi
 # install ansible. ---------------------------------------------------------------------------------
 # install and upgrade ansible in the user's home account.
 runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip3 install ansible --user" - ${user_name}
-runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip3 install --upgrade ansible --user" - ${user_name}
+runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip3 install ansible --upgrade --user" - ${user_name}
+
+# verify ansible installation.
+runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} ansible --version" - ${user_name}
