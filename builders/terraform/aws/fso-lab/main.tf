463517f721be45a987ea782d851a8dd4bb399eb6
Ed Barberis <ed.barberis@gmail.com>
2023-05-30 14:26:25 -0400

    Upgraded Terraform to 1.4.6; AWS to 5.0; and EKS Blueprints to 4.32.0.

--- builders/terraform/aws/fso-lab/main.tf
+++ builders/terraform/aws/fso-lab/main.tf
@@ -232,7 +232,7 @@ module "vm" {
 }
 
 module "eks_blueprints" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.31.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.0"
 
   cluster_name       = local.cluster_name
   cluster_version    = var.aws_eks_kubernetes_version
