8d55da930f610732bb8c2c633833f89848d473a6
Ed Barberis <ed.barberis@gmail.com>
2023-06-28 15:13:31 -0400

    Upgraded Terraform to 1.5.2; AWS to 5.5; and EKS Blueprints to 4.32.1.

--- builders/terraform/aws/fso-lab/main.tf
+++ builders/terraform/aws/fso-lab/main.tf
@@ -136,7 +136,7 @@ data "aws_security_group" "eks_remote" {
 # Modules ------------------------------------------------------------------------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 4.0"
+  version = ">= 5.0"
 
   name = local.vpc_name
   cidr = var.aws_vpc_cidr_block
@@ -166,7 +166,7 @@ module "vpc" {
 
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = ">= 4.17"
+  version = ">= 5.1"
 
   name        = local.security_group_name
   description = "Security group for LPAD VM EC2 instance"
@@ -199,7 +199,7 @@ module "security_group" {
 
 module "vm" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = ">= 5.0"
+  version = ">= 5.1"
 
   name                 = local.vm_name
   ami                  = data.aws_ami.fso_lab_ami.id
@@ -232,7 +232,7 @@ module "vm" {
 }
 
 module "eks_blueprints" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1"
 
   cluster_name       = local.cluster_name
   cluster_version    = var.aws_eks_kubernetes_version
