bcd9154e16299263ff0108ce4b7b4a2896a2e185
Ed Barberis <ed.barberis@gmail.com>
2023-12-08 11:09:26 -0500

    Updated support for Docker Compose 2.23.3.

--- provisioners/scripts/common/install_docker_compose_2.sh
+++ provisioners/scripts/common/install_docker_compose_2.sh
@@ -23,10 +23,10 @@
 #---------------------------------------------------------------------------------------------------
 
 # install docker compose v2 cli. -------------------------------------------------------------------
-dc_release="2.22.0"
+dc_release="2.23.3"
 dc_home="/usr/libexec/docker/cli-plugins"
 dc_binary="docker-compose-linux-x86_64"
-dc_sha256="e849450f1c5c20123aa7d63291e2a818b7a117f2e03e432853ece463dd09e67a"
+dc_sha256="a836e807951db448f991f303cddcc9a4ec69f4a49d58bc7d536cb91c77c04c33"
 
 # create docker cli-plugins directory (if needed).
 mkdir -p ${dc_home}
